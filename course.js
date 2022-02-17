8066cfbbb5638c5d4be6e931d03f67683d0a56ad
savage-sean91 <savage.sean91@gmail.com>
2022-02-16 20:59:55 -0500

    just some commits
    2-16-2022

--- course.js
+++ course.js
@@ -1,16 +1,16 @@
 // course.js - Class route module
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Home page route.
-router.get('/', function (req, res) {
+router.get("/", function (req, res) {
   res.send("Course home page");
-})
+});
 
 // About page route.
-router.get('/about', function (req, res) {
-  res.render('course');
-})
+router.get("/about", function (req, res) {
+  res.render("course");
+});
 
 module.exports = router;
